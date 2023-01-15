8134c0e1dc4f76a97e50974d8d737642e62ccc97
webRavish456 <47687766+webRavish456@users.noreply.github.com>
2023-01-15 18:36:13 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express();
 const httpServer=http.createServer(app);
 const io = new Server(httpServer,{
     cors: {
-        origin:'http://localhost:3000',
+        origin:'*',
     
     }
 })
