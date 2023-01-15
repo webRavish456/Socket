2db153a7ba6ed16b3cc35b49ceb2930876be5891
webRavish456 <47687766+webRavish456@users.noreply.github.com>
2023-01-15 18:35:41 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,52 +2,98 @@ import { Server } from "socket.io";
 import http from "http";
 import express from 'express';
 
+
 const app = express();
 const httpServer=http.createServer(app);
- const io = new Server(httpServer,{
+const io = new Server(httpServer,{
     cors: {
-        origin:'*',
-       
-       
+        origin:'http://localhost:3000',
+    
     }
 })
 
-let users=[];
 
-const addUser= (userData,socketId)=>
+var users=[];
+
+function addUser (userData,socketId)
 {
-   !users.some(user => user.sub == userData.sub) && users.push({...userData, socketId});
+
+     users.push({...userData,socketId});
+
+        
+ for(let i=0;i<users.length;i++)
+ {
+      
+    if(users[i].sub===userData.sub)
+    {
+     
+        if(users[i].socketId===socketId)
+      {     
+          
+           io.emit("getUsers",users);    
+      }
+    
+    else  if(users[i].socketId!==socketId)
+      {     
+            users.splice(i,1);
+             i--;
+      }
+    }
+    
+  else if(users[i].sub!==userData.sub && users[i].socketId!==socketId)
+  {
+               continue;
+  }    
+ }      
 }
+
 const deleteUser=(socketId)=>
 {
+    for(let i=0;i<users.length;i++)
+    {
+        if(users[i].socketId===socketId)
+        {
+              users.splice(i,1);
+              i--;
+             io.emit("getUsers",users);
+            
+        }
+        
+    }
    
-      io.emit("getUsers",users.filter(user=>user.socketId!==socketId)); 
+       
 }
+
 
 const getUser = (userId)=>
 {
-    return users.find(user =>user.sub === userId);
+     return users.find(user =>user.sub === userId);
+    
 }
 
 io.on('connection',(socket)=>{
-     console.log('user connected');
+     
+      
+         socket.on("addUsers",userData=>{ 
+           
+                addUser(userData, socket.id);
+                console.log('User Connected');
 
-     socket.on("addUsers",userData=>{
-         addUser(userData, socket.id);
-         io.emit("getUsers",users);
      })
-
+   
      socket.on('sentMessage',data=>{
 
          const user = getUser(data.receiverId);
          io.to(user.socketId).emit("getMessage",data);
      })
-  socket.on('disconnect',userData=>{
-          
-            deleteUser(socket.id);
-         
-            console.log('User Disconnected');
 
-     });
+     socket.on('disconnect',(userData,socketId)=>
+     {
+         deleteUser(socket.id);
+         console.log('User Disconnected');
+     })
+     
 })
+
+  
 httpServer.listen(9000);
